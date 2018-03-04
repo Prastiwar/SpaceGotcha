34e9181143d64f9fed8a953d14461babe86a316d
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-03-04 14:47:01 +0100

    Visual content update

---
+++ SpaceGotcha/Assets/_Main/Scripts/ColorChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField] float changeValue = 0.1f;
+    
+    float hColor = 0.15f;
+    bool shouldDecrease = false;
+
+    ParticleSystem particles;
+    ParticleSystem.MainModule main;
+
+    void Awake()
+    {
+        particles = GetComponent<ParticleSystem>();
+        main = particles.main;
+    }
+
+    void Update()
+    {
+        main.startColor = Color.HSVToRGB(hColor, 1, 1);
+
+        hColor = Mathf.LerpAngle(hColor, shouldDecrease ? hColor - changeValue : hColor + changeValue, Time.deltaTime);
+        //hColor = Mathf.SmoothDampAngle(hColor, shouldDecrease ? hColor - changeValue : hColor + changeValue, ref hColor, Time.deltaTime);
+
+        if (hColor >= 0.55f)
+        {
+            shouldDecrease = true;
+        }
+        else if (hColor <= 0.15f)
+        {
+            shouldDecrease = false;
+        }
+    }
+}
